1f31e414cee51ee33ed29e724511e2dc8446fbb4
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-12-17 15:23:33 +0700

    config(host): ip server prod

--- src/stores/https/index.js
+++ src/stores/https/index.js
@@ -5,7 +5,7 @@ import router from '@/router'
 const url = [{ dev: 'http://localhost:1688' }, { prod: 'https://vanguardhrm.io.vn' }, { test: '' }]
 
 const env = [{ 1: 'dev' }, { 2: 'prod' }, { 3: 'test' }]
-const environment = 1
+const environment = 2
 
 const https = axios.create({
     baseURL: 'http://157.66.26.244:1688',
